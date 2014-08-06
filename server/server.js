746a978f5bc28d5c9e348d911a0390982927a9fd
Karl Mikkelsen <karl@lockedonhq.com>
2014-08-06 21:11:41 +1000

    move server side render into separate module

--- server/server.js
+++ server/server.js
@@ -1,15 +1,10 @@
 //Server side code to start here
-//expressjs / loopback / etc
+//expressjs
 
-require('node-jsx').install({harmony: true});
-var fs = require("fs");
 var express = require('express');
-var React = require("react");
 var app = express();
 
 var port = process.env.PORT || 8080;
-
-var router = express.Router();
 
 //return static assets first
 app.use(express.static("./public"));
@@ -18,17 +13,8 @@ app.use(express.static("./public"));
 app.use('/api/echo', require('./api/echo'));
 
 //render the app server side
-var clientApp = require("../client/app/app");
-router.get('/', function(req, res) {
-  //fetch the template
-  var template = fs.readFileSync("./client/app.html", {encoding:'utf8'});
-  //render the app
-  var body = React.renderComponentToString(clientApp());
-  //merge body into template
-  res.send(template.replace(/<\/body>/, body + "</body>")); 
-});
-
-app.use('/', router);
+app.use('/', require('./render/render'));
 
 app.listen(port);
+
 console.log('App started goto - http://0.0.0.0:' + port);
