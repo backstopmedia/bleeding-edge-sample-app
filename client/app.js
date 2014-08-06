e9729befe9042b2bce056956fc3d93775d7655f6
Karl Mikkelsen <karl@lockedonhq.com>
2014-08-06 16:13:34 +1000

    Initial commit
    Expresses server side
    serverside rendering
    npm start to build and start
    jsx parser setup

---
+++ client/app.js
@@ -0,0 +1,10 @@
+/** @jsx React.DOM */
+var React = require("react");
+
+var App = React.createClass({
+  render:function(){
+    return <div>Hi Co-Authors!</div>
+  }
+});
+
+module.exports = App;
---
+++ server/server.js
@@ -0,0 +1,30 @@
+//Server side code to start here
+//expressjs / loopback / etc
+
+require('node-jsx').install({harmony: true});
+var fs = require("fs");
+var express = require('express');
+var React = require("react");
+var app = express();
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+
+
+//render the app server side
+var clientApp = require("../client/app.js");
+router.get('/', function(req, res) {
+  //fetch the template
+  var template = fs.readFileSync("./client/app.html", {encoding:'utf8'});
+  //render the app
+  var body = React.renderComponentToString(clientApp());
+  //merge body into template
+  res.send(template.replace(/<\/body>/, body + "</body>")); 
+});
+
+app.use('/', router);
+app.use(express.static("./public"));
+
+app.listen(port);
+console.log('App started goto - http://0.0.0.0:' + port);
