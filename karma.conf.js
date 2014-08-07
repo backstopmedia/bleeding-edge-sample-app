4ec2d2f7acf3f12119463abbd44f3e621dc8c3a6
Tom Hallett <tom@tout.com>
2014-08-07 09:27:26 -0700

    Basic karma-browserify w/ reactify transform working.
    TODO:
    * phantomjs needs a shim because it doesn't have prototype.bind natively
    * fix /server/**/*.js preprocessing

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-//      'client/**/*.js',
+      'client/**/*.js',
       'test/**/*.js'
 //      'server/**/*.js'
     ],
@@ -27,6 +27,15 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'client/**/*.js': ['browserify'],
+      'test/**/*.js': ['browserify']
+//      'server/**/*.js': ['browserify']
+    },
+
+
+    browserify: {
+      debug: true,
+      transform: [ 'reactify' ]
     },
 
 
@@ -55,7 +64,8 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox', 'PhantomJS'],
+//    browsers: ['Chrome', 'Firefox', 'PhantomJS'],
+    browsers: ['Chrome', 'Firefox'],
 
 
     // Continuous Integration mode
