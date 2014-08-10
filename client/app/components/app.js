3fcc0af52b1bfe6024ef295045456c82e635c7cc
Jon Beebe <somethingkindawierd@gmail.com>
2014-08-10 13:07:46 -0500

    Removes SurveyPage from app.js

--- client/app/components/app.js
+++ client/app/components/app.js
@@ -4,18 +4,12 @@ var React = require("react");
 
 var MainHeader = require('./main_header');
 
-var SurveyEditor = require('./survey_editor');
-var PatternLibrary = require('./pattern_library');
-var mockSurveyData = require('../mock_survey_data');
-var SurveyPage = require("../../components/survey/survey_page");
-
 var App = React.createClass({
   render: function () {
     return (
       <div className='app'>
         <MainHeader currentUri='/'/>
         <div className='main-content container-fluid'>
-          <SurveyPage data={mockSurveyData} />
           {this.props.children}
         </div>
       </div>
