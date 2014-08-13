ce9a89f6d607e969b6dca2aa91327ec9bf722dc8
Frankie Bagnardi <f.bagnardi@gmail.com>
2014-08-12 21:55:14 -0700

    initial api implementation

--- server/server.js
+++ server/server.js
@@ -6,15 +6,33 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
-//echo api route
-app.use('/api/echo', require('./api/echo'));
+app.use('/api', require('body-parser').json());
+
+app.use('/api', function(req, res, next){
+    console.error(req.method + " " + req.url + "\n" + require('util').inspect(req.body));
+    try {
+        next();
+
+    } catch(e) {
+        console.error("Request failed " + require('util').inspect(req));
+        console.error(req.method + " " + req.url);
+        res.json(500, {message: "An unknown error occured.  Tell the devs it's " + req.method + " " + req.url});
+    }
+});
+
+app.use('/api/surveys', require('./api/surveys'));
 
 //return static assets
 app.use(express.static('./public'));
 
 //render the app server side
-app.use('/', require('./render/render'));
+if (!process.env.API_ONLY) {
+    app.use('/', require('./render/render'));
+}
 
-app.listen(port);
+if (require.main === module) {
+    console.log('App started goto - http://0.0.0.0:' + port);
+    app.listen(port);
+}
 
-console.log('App started goto - http://0.0.0.0:' + port);
+module.exports = app;
