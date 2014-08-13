7566e58f9d9f399a8d58c6f57dee106301b9622b
Jon Beebe <somethingkindawierd@gmail.com>
2014-08-12 20:32:35 -0500

    Removes test from app.js

--- client/app/components/app.js
+++ client/app/components/app.js
@@ -3,8 +3,6 @@
 var React = require("react");
 
 var MainHeader = require('./main_header');
-var TakeSurvey = require('./take_survey');
-var mockData = require('../mock_survey_data');
 
 var App = React.createClass({
   render: function () {
@@ -13,7 +11,6 @@ var App = React.createClass({
         <MainHeader currentUri='/'/>
         <div className='main-content container'>
           {this.props.children}
-          {new TakeSurvey(mockData)}
         </div>
       </div>
     );
