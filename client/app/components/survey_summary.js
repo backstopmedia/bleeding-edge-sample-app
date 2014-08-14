c9298f52ee883c7b478dcc7f02a9114525189f94
Simon Højberg <r.hackr@gmail.com>
2014-08-13 20:01:38 -0400

    Hook up AppRouter with page level components

---
+++ client/app/components/survey_summary.js
@@ -0,0 +1,11 @@
+/** @jsx React.DOM */
+
+var React = require("react");
+
+var SurveySummary = React.createClass({
+  render: function(){
+    return <div>Survey Summary</div>
+  }
+});
+
+module.exports = SurveySummary;
