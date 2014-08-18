1d617f6effdf9b0b258347e3f83e19886f756ed8
Richard Feldman <richard.t.feldman@gmail.com>
2014-08-18 00:19:54 -0700

    Wire up the store and the dispatcher on app startup.

--- client/app/components/app.js
+++ client/app/components/app.js
@@ -2,6 +2,22 @@
 
 var React = require("react");
 var MainHeader = require('./main_header');
+var SurveyStore = require("../flux/SurveyStore");
+var Dispatcher = require("../flux/Dispatcher");
+var SurveyConstants = require("../flux/SurveyConstants");
+
+// Wire up the SurveyStore with the action dispatcher
+Dispatcher.register(function(payload) {
+  switch(payload.actionType) {
+    case SurveyConstants.SAVE_SURVEY:
+      SurveyStore.saveSurvey(payload.survey);
+      break;
+
+    case SurveyConstants.DELETE_SURVEY:
+      SurveyStore.deleteSurvey(payload.id)
+      break;
+  }
+});
 
 var App = React.createClass({
   render: function () {
