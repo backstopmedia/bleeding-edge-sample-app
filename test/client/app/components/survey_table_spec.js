da6a16167a506bb6854e41216f6e5291acbd083f
Frankie Bagnardi <f.bagnardi@gmail.com>
2014-08-18 23:34:43 -0700

    fix missing #4 compatibility

--- test/client/app/components/survey_table_spec.js
+++ test/client/app/components/survey_table_spec.js
@@ -11,24 +11,24 @@ var data = [{
   uri: "/surveys/287",
   editUri: "/surveys/287/edit",
   title: "Game of Thrones",
-  publishedDate: new Date(2014, 07, 1),
-  modifiedDate: new Date(2014, 07, 6),
+  createdAt: new Date(2014, 07, 1),
+  updatedAt: new Date(2014, 07, 6),
   activity: []
 }, {
   id: "345",
   uri: "/surveys/345",
   editUri: "/surveys/345/edit",
   title: "Favorite Harry Potter Character",
-  publishedDate: new Date(2014, 06, 17),
-  modifiedDate: new Date(2014, 07, 10),
+  createdAt: new Date(2014, 06, 17),
+  updatedAt: new Date(2014, 07, 10),
   activity: []
 }, {
   id: "378",
   uri: "/surveys/378",
   editUri: "/surveys/378/edit",
   title: "Do You Understand Net Neutrality?",
-  publishedDate: new Date(2014, 06, 04),
-  modifiedDate: new Date(2014, 06, 29),
+  createdAt: new Date(2014, 06, 04),
+  updatedAt: new Date(2014, 06, 29),
   activity: []
 }];
 
