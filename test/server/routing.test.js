b3e848685f760c9d53de2c3624a95ae4e1b0fdef
Karl Mikkelsen <karl@karlmikko.com>
2014-08-20 21:07:55 +1000

    server test joined together

---
+++ test/server/routing.test.js
@@ -0,0 +1,32 @@
+var request = require('supertest');
+var cheerio = require('cheerio');
+var app = require('../../server/server.js');
+var expect = require('expect.js');
+
+request = request(app);
+
+describe("serverside routing", function(){
+  it("should render the AddSurvey component for the /add_survey path", function(done){
+
+    request.get('/add_survey')
+      .expect(200)
+      .end(function (err, res) {
+        var doc = cheerio.load(res.text);
+        expect(doc("title").html()).to.be("Add Survey to SurveyBuilder");
+        expect(doc(".main-content .survey-editor").length).to.be(1);
+        done();
+      });
+  });
+
+
+
+  it("should render a 404 page for an invalid route", function(done){
+      request.get('/not-found-route')
+      .expect(404)
+      .end(function (err, res) {
+        var doc = cheerio.load(res.text);
+        expect(doc("body").html()).to.contain("The Page you were looking for isn&apos;t here!");
+        done();
+      });      
+  });
+});
